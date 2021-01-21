dccad0b4fdc95c47fc8bfd0d018f097d032a4b1c
chenghch <chenghch@yonyou.com>
2021-01-21 11:00:13 +0800

    初始化元素

--- src/main/java/com/timory/utils/SpiderUtil.java
+++ src/main/java/com/timory/utils/SpiderUtil.java
@@ -7,6 +7,10 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+
 
 /*
  * 爬虫工具
@@ -15,19 +19,26 @@ public class SpiderUtil {
     /*
      * @url 爬虫地址
      * @tag 图片标签
-     * @store分类存储地址
+     * @store 分类存储地址
      * */
-    public static void DownPIC(String url, String tag, String store) {
+    public static void DownPIC(String url, String tag, String store) throws IOException, NoSuchAlgorithmException, KeyManagementException {
         String result1 = HttpUtil.get(url);
         Document document = Jsoup.parse(result1);
-        Elements masonry_brick_ = document.select(tag);
-        for (Element link : masonry_brick_) {
+//        Document document = Jsoup.connect(url).get();
+        Elements masonry_brick = document.select(tag);
+        int i = 0;
+        for (Element link : masonry_brick) {
+            i++;
             String src = link.attr("src");
-            HttpUtil.downloadFile(src, FileUtil.file(store));
+            String alt = link.attr("alt");
+            String last = src.substring(src.lastIndexOf("."), src.length());
+            String picName = i + "-" + alt + last;
+            HttpUtil.downloadFile(src, FileUtil.file(store + picName));
+            System.out.println("Done:" + picName);
         }
     }
 
-    public static void main(String[] args) {
-        DownPIC("https://www.mmonly.cc/mmtp/", ".masonry_brick img", "D://SPIDER//mmonly");
+    public static void main(String[] args) throws IOException, KeyManagementException, NoSuchAlgorithmException {
+        DownPIC("https://www.mmonly.cc/mmtp/", ".masonry_brick img", "D://SPIDER//mmonly//");
     }
 }
